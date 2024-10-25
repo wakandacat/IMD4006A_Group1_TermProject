61fd7241a32f8a1f82579701fca68e69841d414e
Ellena Tzavelas <wakandacat@gmail.com>
2024-10-25 13:48:34 -0400

    item spawner works, spawns lower point values more often

--- Assets/SpawnItems.cs
+++ Assets/SpawnItems.cs
@@ -1,6 +1,9 @@
 using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Windows;
 
@@ -8,7 +11,7 @@ using UnityEngine.Windows;
 public class SpawnItems : MonoBehaviour
 {
     public item[] prefabList;     //array of gameObjects of class 'item', these should be prefabs
-    public Vector3[] spawnPoints = new[] { new Vector3(20f, -1f, 20f), new Vector3(35f, 0f, 55f), new Vector3(15f, 0f, 60f) };
+    public Vector3[] spawnPoints = new[] { new Vector3(20f, 0f, 25f), new Vector3(35f, 0f, 55f), new Vector3(20f, 0f, 25f), new Vector3(51f, 0f, 53f), new Vector3(74f, 0f, 70f), new Vector3(72f, 0f, 68f) };
     
     //bucket areas
     public GameObject[] areas;
@@ -45,38 +48,114 @@ public class SpawnItems : MonoBehaviour
 
             int tempNum = -1;
 
+            //catch for any items outside range
+            bool itemInArea = false;
+
+            //make a case for every area
             switch (currArea)
             {
                 case 0:
-                    Debug.Log("area0");
-
+                    //Debug.Log("area0");
+                    itemInArea = true;
                     //choose between the possible prefabs for this area
-                    //tempNum = Random.Range(0, 1); //will return either 0 or 1
-                    tempNum = 0;
-                    setPosition(tempNum, spawnPoints[i]);
-                    Instantiate(prefabList[tempNum]);     //instantiate instance of item to scene
+                    tempNum = getItem(currArea);
                     break;
                 case 1:
                     Debug.Log("area1");
-
+                    itemInArea = true;
                     //choose between the possible prefabs for this area
-                    tempNum = Random.Range(0, 2); //will return either 0 or 1
-
-                    setPosition(tempNum, spawnPoints[i]);
-                    Instantiate(prefabList[tempNum]);     //instantiate instance of item to scene
+                    tempNum = getItem(currArea);
+                    break;
+                case 2:
+                    //Debug.Log("area2");
+                    itemInArea = true;
+                    //choose between the possible prefabs for this area
+                    tempNum = getItem(currArea);
                     break;
                 default:
-                    Debug.Log("nahhhhhhhhhhhhhh");
+                    //Debug.Log("nah");
                     break;
             }
-        }
 
+            if (itemInArea == true)
+            {
+                setPosition(tempNum, spawnPoints[i], currArea);
+                Instantiate(prefabList[tempNum]);     //instantiate instance of item to scene
+                itemInArea = false;
+            }
+
+        }
     }
 
     //set the current spawnPosition to be the position of the prefab at the random num index
-    public void setPosition(int num, Vector3 pos)
+    public void setPosition(int num, Vector3 pos, int areaIndex)
     {
-        prefabList[num].transform.position = pos;
+        //vary the height by the point value, so higher point value items are deeper in the terrain
+        float yPos = areas[areaIndex].transform.localScale.y - (areas[areaIndex].transform.localScale.y / prefabList[num].itemPtValue);
+
+        prefabList[num].transform.position = new Vector3(pos.x, -yPos, pos.z);
+    }
+
+    //get all the items that can spawn in an area, and pick one
+    public int getItem(int areaIndex)
+    {
+        List<int> indecesToPickFrom = new List<int>();
+        int chosenItem = -1;
+        List<int> prefabIndecesForArea = new List<int>(); //use a list to append the indeces to
+
+        for (int i = 0; i < prefabList.Length; i++)
+        {
+            // Check if the current prefab's spawnAreas contains the areaIndex
+            if (Array.Exists(prefabList[i].spawnAreas, elem => elem == areaIndex))
+            {
+                prefabIndecesForArea.Add(i); //add the index to the list
+            }
+        }
+
+        //convert it back to an array
+        int[] prefabIndecesArray = prefabIndecesForArea.ToArray();
+        //Debug.Log("list of prefabs in this area" + string.Join(", ", prefabIndecesArray));
+
+        //determine weights of items to pick randomly
+        //get ptvalue of each item in the area (ex. 5, 3, 1)
+        //multiply them all together to get a sum (ex. 5x3x1 = 15)
+        //divide the sum by each ptvalue to get the amount of instances of that item in an area to pick randomly from (ex. 15/5 = 3, 15/3 = 5, 15/1 = 15)
+        //pick a random nunber from the array (ex. array[5,5,5,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+
+        int sumPts = 1;
+        int tempVal = 0;
+
+        //multiply the point values together
+        for (int i = 0; i < prefabIndecesArray.Length; i++)
+        {
+            sumPts = sumPts * prefabList[prefabIndecesArray[i]].itemPtValue;
+        }
+
+        //Debug.Log("sum" + sumPts);
+
+        for (int i = 0; i < prefabIndecesArray.Length; i++)
+        {
+            tempVal = sumPts / prefabList[prefabIndecesArray[i]].itemPtValue; 
+            
+            //for the size of tempVal, add that many instances to the array to pick from
+            for (int x = 0; x < tempVal; x++)
+            {
+                //append the index of that item a bunch of times
+                indecesToPickFrom.Add(prefabIndecesArray[i]);
+            }
+
+        }
+
+        //convert it back to an array
+        int[] randomArr = indecesToPickFrom.ToArray();
+        //Debug.Log("indeces to pick from" + string.Join(", ", randomArr));
+
+        //pick an item index and return it
+        chosenItem = randomArr[UnityEngine.Random.Range(0, randomArr.Length)];
+        //Debug.Log("chosen item" + chosenItem);
+
+        return chosenItem;
+
     }
 
 }
--- Assets/item.cs
+++ Assets/item.cs
@@ -10,12 +10,13 @@ public class item : MonoBehaviour
     //these could all potentially be made private variables
     public string itemName;         //item's name
     public float itemWeight;        //item's weight, will be adding to/affect the crab's movement speed and claw y position (only slightly)
-    //public Vector3 itemPosition;    //item's position in the world space
-    private int itemPtValue;        //item's point value, will be added and used to determine the house's overall point value
-    public int spawnArea;
+    public int itemPtValue;        //item's point value, will be added and used to determine the house's overall point value
+    public int[] spawnAreas;        //array of the indeces of all areas in which it can spawn (ex. can spawn in area1 and area2, then spawnAreas = [1, 2])
+    public bool specialItem;        //true if only an NPC given item
+    public bool breakable;          //true if it is a breakable item (clamshell)
 
     //public void positionItem()
     //{
-    //    itemPrefab.transform.position = itemPosition;   //this code may need to be altered to work
+    //    
     //}
 }
