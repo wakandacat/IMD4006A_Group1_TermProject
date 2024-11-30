843afda0f834acfce1e00776b81ed3237db0fbad
Ellena Tzavelas <wakandacat@gmail.com>
2024-11-29 23:21:02 -0500

    added random npcs with new npc models and materials

--- Assets/HomeScript.cs
+++ Assets/HomeScript.cs
@@ -1,8 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
-//using static UnityEditor.Progress;
 
 public class HomeScript : MonoBehaviour
 {
@@ -11,31 +11,51 @@ public class HomeScript : MonoBehaviour
 
     //home area
     public GameObject home;
-    public GameObject npc;
+    private int numNPCs = 10; //total amount of NPCs in game
+    public GameObject[] npcs;
     public GameObject[] items;
+    public Material[] NPCMats;
     public GameObject textPrefab;
     public GameObject camera;
+    public string[] NPCText = new[] { "What a nice home!", "OOOOOO!", "Nice place! Here ya go!", "Found this for you!", "You should be mayor!", "Whoa!" , "What a beautiful home!" , "I love it!", "Amazing place!" };
 
-    //total point value
+    //total point value on castle
     int totalPts = 0;
 
-    //an array of decorate positions on the castle
-    Vector3[] spawnPoints = new[] { new Vector3(4.5f, 5f, 66.2f), new Vector3(4.5f, 6.4f, 66f), new Vector3(6.55f, 5f, 68.2f), new Vector3(6.55f, 6.4f, 68.2f), new Vector3(6.2f, 5f, 67f), new Vector3(6.2f, 6.42f, 67f), new Vector3(6.55f, 7.4f, 68.2f), new Vector3(4.5f, 7.4f, 66f), new Vector3(6.55f, 7.4f, 68.2f) };
-
     //npc spawn
-    NPCClass[] npcArr;
+    List<NPCClass> npcArr = new List<NPCClass>();
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //create NPC instances
-        NPCClass npc1 = new NPCClass(npc, items[0], 5, new Vector3(4f, 3f, 53f), new Vector3(0f, 0f, 0f), textPrefab, "What a nice home!");
-        NPCClass npc2 = new NPCClass(npc, items[1], 8, new Vector3(4f, 3f, 45f), new Vector3(0f, 25f, 0f), textPrefab, "OOO, fancy!");
-        NPCClass npc3 = new NPCClass(npc, items[2], 11, new Vector3(15f, 3f, 70f), new Vector3(0f, -90f, 0f), textPrefab, "Nice place! Here ya go!");
-        NPCClass npc4 = new NPCClass(npc, items[3], 15, new Vector3(18f, 3f, 65f), new Vector3(0f, -120f, 0f), textPrefab, "Found this for you!");
+        //create NPC insatnces to spawn later
+        for (int i = 0; i < numNPCs; i++)
+        {
+            //get a bunch of random values
+            int currNPC = UnityEngine.Random.Range(0, npcs.Length);
+            int currText = UnityEngine.Random.Range(0, NPCText.Length);
+            int currPts = UnityEngine.Random.Range(3, 5);
+            int currMat = UnityEngine.Random.Range(0, NPCMats.Length);
+            int currItem = UnityEngine.Random.Range(0, items.Length - 1);
+            float currX = UnityEngine.Random.Range(4f, 30f);
+            float currZ = UnityEngine.Random.Range(50f, 70f);
+            float scale = UnityEngine.Random.Range(0.5f, 1.0f);
 
-        npcArr = new NPCClass[] { npc1, npc2, npc3, npc4 };
+            if (i == 0) //vary the amount of points needed to spawn the next NPC
+            {
+                npcArr.Add(new NPCClass(npcs[currNPC], items[currItem], currPts, new Vector3(4f, 3f, 53f), textPrefab, NPCText[currText], NPCMats[currMat], scale));
+            }
+            else if (i == numNPCs - 1) //the very last NPC -> the very last item
+            {
+                npcArr.Add(new NPCClass(npcs[currNPC], items[items.Length-1], npcArr[i - 1].ptsToSpawn + currPts, new Vector3(currX, 3f, currZ), textPrefab, NPCText[currText], NPCMats[currMat], scale));
+            }
+            else //the rest of the npcs
+            {
+                npcArr.Add(new NPCClass(npcs[currNPC], items[currItem], npcArr[i - 1].ptsToSpawn + currPts, new Vector3(currX, 3f, currZ), textPrefab, NPCText[currText], NPCMats[currMat], scale));
+            }
+
+        }
 
     }
 
@@ -45,20 +65,21 @@ public class HomeScript : MonoBehaviour
 
         //Debug.Log(totalPts);
         //check if the total point value is enought to spawn NPC
-        for (int i = 0; i < npcArr.Length; i++)
+        for (int i = 0; i < npcArr.Count; i++)
         {
             if (totalPts >= npcArr[i].ptsToSpawn)
             {
-                npcArr[i].spawnNPC(camera.transform.eulerAngles.y);
-                npcArr[i].updateRotation(camera.transform.eulerAngles.y);
+                npcArr[i].spawnNPC();
+                npcArr[i].updateRotation();
             }
         }
 
     }
 
     public void decorateItem(GameObject droppedItem)
-    {       //increment total points
-            totalPts = totalPts + droppedItem.GetComponent<item>().itemPtValue;
+    {       
+        //increment total points
+        totalPts = totalPts + droppedItem.GetComponent<item>().itemPtValue;
         
     }
 
--- Assets/NPCClass.cs
+++ Assets/NPCClass.cs
@@ -13,39 +13,51 @@ public class NPCClass : MonoBehaviour
     private Vector3 spawnRot;
     private GameObject text;
     private string textDesc;
+    private Material material;
+    private float scale;
 
 
     //constructor
-    public NPCClass(GameObject npcModel, GameObject item, int ptsToSpawn, Vector3 spawnPos, Vector3 spawnRot, GameObject text, string textDesc)
+    public NPCClass(GameObject npcModel, GameObject item, int ptsToSpawn, Vector3 spawnPos, GameObject text, string textDesc, Material mat, float scale)
     {
         this.npcModel = npcModel;
         this.item = item;
         this.ptsToSpawn = ptsToSpawn;
         this.spawnPos = spawnPos;
-        this.spawnRot = spawnRot;
         this.text = text;
         this.textDesc = textDesc;
+        this.material = mat;
+        this.scale = scale;
     }
 
-    public void spawnNPC(float camRotY)
+    public void spawnNPC()
     {
         if (hasSpawned == false)
         {
-            this.npcModel = Instantiate(npcModel, spawnPos, Quaternion.Euler(0f, camRotY - 180, 0f));
+            this.npcModel = Instantiate(npcModel, spawnPos, Quaternion.identity);
+            //change the body and the two claws to have the new colour
+            this.npcModel.transform.GetChild(0).GetComponent<MeshRenderer>().material = material;
+            this.npcModel.transform.GetChild(1).GetComponent<MeshRenderer>().material = material;
+            this.npcModel.transform.GetChild(2).GetComponent<MeshRenderer>().material = material;
+            this.npcModel.transform.localScale = new Vector3(scale, scale, scale);
+
             GameObject newItem = Instantiate(item, new Vector3(spawnPos.x + 1f, spawnPos.y, spawnPos.z + 1f), Quaternion.identity);
             newItem.tag = "specialitem";
-            this.text = Instantiate(text, new Vector3(spawnPos.x, spawnPos.y + 1.5f, spawnPos.z), Quaternion.Euler(0f, camRotY, 0f));
+            this.text = Instantiate(text, new Vector3(spawnPos.x, spawnPos.y + 1.5f, spawnPos.z), Quaternion.identity);
             this.text.GetComponent<TextMesh>().text = textDesc;
             hasSpawned = true;
         }
     }
 
-    public void updateRotation(float camRotY)
+    //ensure the NPCs are always looking at the player
+    public void updateRotation()
     {
+        Transform player = GameObject.Find("CrabParentObj").transform;
+
         if (hasSpawned == true)
         {
-            this.text.transform.rotation = Quaternion.Euler(0f, camRotY, 0f);
-            this.npcModel.transform.rotation = Quaternion.Euler(0f, camRotY - 180, 0f);
+            this.text.transform.LookAt(player);
+            this.npcModel.transform.LookAt(player);
         }
     }
 }
