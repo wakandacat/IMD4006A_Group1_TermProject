c923fb804bd9acc72af699663cdbad7f29f68ec1
linajaouani <linajaouani2019@gmail.com>
2024-12-04 18:55:18 -0500

    pauseMneu changes
    added pause menu and some controller functionality

--- Assets/PauseMenuControls.cs
+++ Assets/PauseMenuControls.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class PauseMenuControls : MonoBehaviour
 {
     public static bool pause = false;
+    public GameObject audioManager;
     // Start is called before the first frame update
     public GameObject pauseMenu;
+
+    AudioSource[] audioSources;
     void Start()
     {
-        
+         audioSources = audioManager.GetComponents<AudioSource>();
+        Debug.Log(audioSources.Length);
     }
 
     // Update is called once per frame
@@ -18,33 +23,43 @@ public class PauseMenuControls : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            if(pause == false)
+            if (pause == false)
             {
                 unpauseGame();
             }
             else
             {
-                
+
                 pauseGame();
             }
         }
+
     }
 
     public void pauseGame()
     {
         Time.timeScale = 0f;
         pauseMenu.SetActive(true);
+        
+
+        
+        for(int i=0; i < audioSources.Length; i++)
+        {
+            audioSources[i].Pause();
+            Debug.Log("HELLO");
+        }
+
         pause = false;
     }
     public void unpauseGame()
-    {
-        Time.timeScale = 1f;
+    {        
         pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
         pause = true;
-
     }
     public void returnToMainMenu()
     {
         SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
     }
 }
