93abb2c2b7588437d216613f309517ad149930c8
linajaouani <linajaouani2019@gmail.com>
2024-12-05 20:18:38 -0500

    Added props to scene

--- Assets/EndScreen.cs
+++ Assets/EndScreen.cs
@@ -8,7 +8,7 @@ public class EndScreen : MonoBehaviour
 {
     public GameObject EndScreenFirstButton;
     public GameObject EndScreenPanel;
-    private int numNPCs = 1;
+    private int numNPCs = 10;
 
     public int npcCounter = 0;
     private bool isFinished = false;
